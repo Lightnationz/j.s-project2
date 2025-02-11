339d1057cfa3beef7e5e4eef4e6a89cbb3084dc4
Nnamani Samuel Ifechukwu <samuelifechukwu656@gmail.com>
2025-02-11 09:52:57 +0100

    mini-project
    mini-project

--- project.js
+++ project.js
@@ -1,8 +1,16 @@
 const cartbagbtn = document.querySelector('.cartbagbtn')
-cartbagbtn.addEventListener('', (e)=>{
-    e.target.innerHTML = e.type
-console.log(e.type);
+const navbar = document.querySelector('.navbar')
 
-    navbar.classList.remove('akpu')
+const nav = document.querySelector('.nav')
+const closenav = document.querySelector('.closenav')
+
+cartbagbtn.addEventListener('click', (e)=>{
+    // e.target.innerHTML = e.type
+// console.log(e.type);
+
     navbar.classList.toggle('shownav')
+    // navbar.classList.remove('shownav')
+})
+closenav.addEventListener('click', (B)=>{
+    navbar.classList.remove('shownav')
 })
